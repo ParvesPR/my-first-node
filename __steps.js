c803fdc2e29c50ac699428ad73d600eccdf0e1d0
ParvesPR <parves2585@gmail.com>
2022-04-20 09:53:48 +0600

    first commit

---
+++ __steps.js
@@ -0,0 +1,11 @@
+/* 
+* 1. Create a folder(manually or mkdir)
+* 2. open cmd and to that folder 
+* 3. npm init
+* 4. npm install express
+5. Create index.js file
+*   -------------------
+* For express/node server
+*         --------------------
+* 1.
+*/
